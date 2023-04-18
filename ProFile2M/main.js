ee03576448fd4f6a05d135a49442b974826b0eea
datbietvietcode <ngthedat973@gmail.com>
2023-04-19 01:27:08 +0700

    first commit

---
+++ ProFile2M/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Web Disigner .","Full Stack Developer .","Vietnamese Programmer ."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
---
+++ ProFile3M/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".multiple-text", {
+    strings:["","Full Stack Developer ."," From HaNoi Vietnam ."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".multiple-text1", {
+    strings:[""," Những Cái Loại Không Làm Mà Đòi Có Ăn Thì Chỉ Có Ăn Cái Đầu Cứt, Ăn Buồi Mà Thôi ."],
+    typeSpeed:200,
+    BackSpeed:60,
+    loop:true
+})
